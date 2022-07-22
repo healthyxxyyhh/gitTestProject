d35f22595a7712947aca44d880b900207263c483
xuyihui <1070874097@qq.com>
2022-07-22 13:44:40 +0800

    git commit test

---
+++ .idea/src/depolyLamp.java
@@ -0,0 +1,52 @@
+package algs4;
+
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class depolyLamp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        //PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++){
+            int roadLong = sc.nextInt();
+            String str = sc.next();
+            System.out.println(lampSum(roadLong,str));
+        }
+
+        }
+    public static int lampSum(int l,String r){
+        int sum = 0;
+        for(int i=0; i<l; i++){
+            if(r.charAt(i) == '.'){
+                sum ++;
+                i+=2;
+            }
+        }
+        return sum;
+       // int M = sc.nextInt();
+//        for(int i = 0; i < N; i++){
+//            int roadLong = sc.nextInt();
+//            String str = sc.next();
+//            System.out.println(str + "what");
+//            char[] charArray = str.toCharArray();
+//            int count = 0;
+//            int need = 0;
+//            assert (str.length()== roadLong);
+//            for(int j = 0; j < roadLong; j++){
+//                while(charArray[j] == '.' ){
+//                    ++count;
+//                    j++;
+//                    if(j == roadLong){ break};
+//                }
+//                need += (count % 3 == 0 ? count/3:count / 3 + 1);
+//                    count = 0;
+//            }
+//            writer.println(need);
+//        }
+//        writer.flush();
+
+    }
+
+}
